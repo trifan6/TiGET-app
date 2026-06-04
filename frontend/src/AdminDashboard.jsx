bc7c8f4f1b5c9b254fbd40e9bad807f2a13da3d1
trifan6 <trifanstefan66@gmail.com>
2026-06-04 08:58:32 +0300

    changed drawer size a bit

--- frontend/src/AdminDashboard.jsx
+++ frontend/src/AdminDashboard.jsx
@@ -625,7 +625,7 @@ export default function AdminDashboard({ onLogout }) {
                 position: "absolute",
                 top: "-20px",
                 right: "-43px",
-                width: "480px",
+                width: "680px",
                 height: "800px",
                 backgroundColor: "rgba(21, 21, 27, 0.65)",
                 backdropFilter: "blur(16px)",
